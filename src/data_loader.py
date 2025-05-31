22f3dbddb7bfd5b7ed7ae8456dc0f90e96d3eb83
NyasaVishwas <nyasaavishwass@gmail.com>
2025-05-31 19:37:30 +0530

    updated data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -8,9 +8,10 @@ def basic_cleaning(df):
     # Convert ratings to numeric, fill NaNs etc.
     df['product_rating'] = pd.to_numeric(df['product_rating'], errors='coerce')
     df['overall_rating'] = pd.to_numeric(df['overall_rating'], errors='coerce')
-    df['product_rating'].fillna(df['product_rating'].mean(), inplace=True)
-    df['overall_rating'].fillna(df['overall_rating'].mean(), inplace=True)
-    df['brand'].fillna("Unknown", inplace=True)
+    df['product_rating'] = df['product_rating'].fillna(df['product_rating'].mean())
+    df['overall_rating'] = df['overall_rating'].fillna(df['overall_rating'].mean())
+    df['brand'] = df['brand'].fillna("Unknown")
+
     
     # Extract main_category
     df['main_category'] = df['product_category_tree'].apply(lambda x: x.split('>>')[0].strip() if pd.notnull(x) else "Unknown")
